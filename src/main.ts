59c50b281b8e4ec2bc27cbf065909cec42c14465
IvanRozb <ivanrozb4225@gmail.com>
2024-04-18 20:21:45 +0300

    Initial

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import VueKonva from 'vue-konva';
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueKonva)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+124
+  </main>
+</template>

7edb8c84a7f669852ece73c2c5ac9cc3b102b382
IvanRozb <ivanrozb4225@gmail.com>
2024-04-20 21:49:42 +0300

    separate blocks

---
+++ src/components/websocket-section/buttons-tab.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+defineProps<{
+  startWebSocket: (e: MouseEvent) => void
+  stopWebSocket: (e: MouseEvent) => void
+  reset: (e: MouseEvent) => void
+  isWebSocketOpen: boolean
+}>()
+</script>
+
+<template>
+  <div class="buttons">
+    <button class="button button-start" @click="startWebSocket" :disabled="isWebSocketOpen">
+      Start
+    </button>
+    <button class="button button-stop" @click="stopWebSocket" :disabled="!isWebSocketOpen">
+      Stop
+    </button>
+    <button class="button button-reset" @click="reset">Reset</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.buttons {
+  display: flex;
+  justify-content: space-between;
+
+  margin-bottom: 32px;
+
+  .button {
+    all: unset;
+
+    padding: 12px 20px;
+    border-radius: 8px;
+
+    color: white;
+    cursor: pointer;
+
+    &[disabled] {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+  }
+
+  .button-start {
+    background-color: green;
+  }
+
+  .button-stop {
+    background-color: red;
+  }
+
+  .button-reset {
+    background-color: orange;
+  }
+}
+</style>
---
+++ src/components/websocket-section/transactions-table.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { ITransaction } from '@/components/websocket-section/interfaces/ITransaction'
+
+defineProps<{
+  transactions: ITransaction[]
+  totalAmount: number
+}>()
+</script>
+
+<template>
+  <h3 class="total">Total: {{ totalAmount.toFixed(8) }} BTC</h3>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>From</th>
+        <th>To</th>
+        <th class="table__sum">Sum</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(transaction, index) in transactions" :key="index">
+        <td>{{ transaction.sender }}</td>
+        <td>{{ transaction.recipient }}</td>
+        <td>{{ transaction.amount.toFixed(8) }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped lang="scss">
+.total {
+  text-align: center;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: fixed;
+
+  th,
+  td {
+    padding: 8px;
+
+    width: 40%;
+
+    border: 1px solid #ddd;
+
+    text-align: left;
+    word-break: break-word;
+  }
+
+  .table__sum {
+    width: 20%;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+}
+</style>

3aac5552ed020a03fcd6466d861979e0484b0260
IvanRozb <ivanrozb4225@gmail.com>
2024-04-21 14:44:31 +0300

    added block node

---
+++ src/components/interactive-workspace/block-node.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import TextNode from '@/components/interactive-workspace/text-node.vue'
+import type { IBlock } from '@/components/interactive-workspace/interfaces/IBlock'
+
+defineProps<{
+  block: IBlock
+}>()
+</script>
+
+<template>
+  <v-group
+    :config="{
+      draggable: true,
+      id: block.id,
+      x: block.x,
+      y: block.y,
+      scaleX: block.scaleX,
+      scaleY: block.scaleY
+    }"
+  >
+    <v-rect
+      :config="{
+        ...block,
+        x: undefined,
+        y: undefined,
+        scaleX: undefined,
+        scaleY: undefined,
+        id: `${block.id}-rect`
+      }"
+    >
+    </v-rect>
+    <text-node :id="block.id" :text="block.text" />
+  </v-group>
+</template>
